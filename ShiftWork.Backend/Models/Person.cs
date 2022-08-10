f61ee6b29cf81c1a06755602f3cf21b666bc4a81
williamag929@gmail.com <williamag929@gmail.com>
2022-08-10 19:24:30 -0400

    nuevo modelo person

---
+++ ShiftWork.Backend/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace ShiftWork.Backend.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DocumentNumber { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Email { get; set; }   
+        public int PhoneNumber { get; set; }
+    }
+}
