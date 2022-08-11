ea6fc98ff5287b19f4d013a2d28066e136f89b39
jose moncada <andresmonk2010@hotmail.com>
2022-08-10 19:05:08 -0500

    Create Class

---
+++ ShiftWork.Backend/Models/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace ShiftWork.Backend.Models
+{
+    public class Task
+    {
+        public int IdTask { get; set; }
+        public string TaskName { get; set; }
+        public string Comment { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
