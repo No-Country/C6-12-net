14cea6d37aba61278b9c1f85b7101d6e622e7b18
williamag929@gmail.com <williamag929@gmail.com>
2022-08-10 23:33:18 -0400

    Controller person, table person created

---
+++ ShiftWork.Backend/Data/ShiftWorkContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ShiftWork.Backend.Models;
+
+namespace ShiftWork.Backend.Data
+{
+    public class ShiftWorkContext : DbContext
+    {
+        public ShiftWorkContext (DbContextOptions<ShiftWorkContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ShiftWork.Backend.Models.Person> Person { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().ToTable("Person");
+        }
+
+    }
+}
