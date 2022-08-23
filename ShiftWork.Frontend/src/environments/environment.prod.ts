4710c0bf795b081129ef4631a23ab6833a4bd33f
williamag929@gmail.com <williamag929@gmail.com>
2022-08-23 11:18:41 -0400

    prod enviroment

--- ShiftWork.Frontend/src/environments/environment.prod.ts
+++ ShiftWork.Frontend/src/environments/environment.prod.ts
@@ -1,9 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl : 'https://localhost:44318/',
-  auth:{
-    domain: 'dev-ht5zjk2x.us.auth0.com',
-    clientId: 'xX6WLqgD6UgYRjZHKtxD5tlkgwVaqzBk',
-    redirectUri: window.location.origin,
-  }
+  apiUrl : any,
+  auth:any
 };
