e62313de18e4bd12c0194f1bfeb50879e48e89c6
Maximiliano Palermo <maxi.palermo.racing@gmail.com>
2022-08-28 17:56:57 -0300

    Subo cambios de TaskShift ".html", ".component.ts" y ".Service.ts"

--- ShiftWork.Frontend/src/app/components/dashboard/task-shift/task-shift.component.ts
+++ ShiftWork.Frontend/src/app/components/dashboard/task-shift/task-shift.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TaskShiftModel } from 'src/app/Model/TaskShift';
+import { TaskShiftService } from '../../../service/task-shift.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-task-shift',
@@ -7,9 +12,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskShiftComponent implements OnInit {
 
-  constructor() { }
+  taskShift : TaskShiftModel = new TaskShiftModel();
 
-  ngOnInit(): void {
+  TaskShift : any;
+
+  constructor(private TaskShiftService: TaskShiftService,
+    public _router : Router,
+    public _location: Location ) { }
+
+    ngOnInit(): void
+  {
+    this.getTaskShift();
+    this.taskShift.taskShiftId = 0;
   }
 
+  private getTaskShift()
+  {
+    this.TaskShiftService.GetTaskShift().subscribe(
+      (data) => {
+        this.TaskShift = data;
+        console.log('TaskShift', data);
+      }
+    );
+  }
+
+  saveData( form: NgForm)
+  {
+    console.log(this.taskShift);
+    console.log(this.TaskShift.taskShiftId);
+    if (form.invalid){
+      console.log('Formulario Invalido');
+      return;
+    }
+    if (this.taskShift.taskShiftId > 0)
+    {
+      this.TaskShiftService.PutTaskShift(this.taskShift)
+      .subscribe(
+        resp => console.log(resp)
+      );
+    }
+    else
+    {
+      this.TaskShiftService.PostTaskShift(this.taskShift)
+        .subscribe(
+          resp => console.log(resp)
+        );
+    }
+    this.taskShift = new TaskShiftModel();
+  }
+
+  selectedTaskShift(taskShift: TaskShiftModel)
+  {
+    this.taskShift = taskShift;
+  }
+
+  deleteTaskShift(taskShift: TaskShiftModel)
+  {
+    console.log(taskShift);
+    this.TaskShiftService.DeleteTaskShift(taskShift.taskShiftId).subscribe();
+    this.getTaskShift();
+  }
+
+  refresh(): void
+  {
+    this._router.navigateByUrl("/refresh", { skipLocationChange: true }).then(() =>
+    {
+	  console.log(decodeURI(this._location.path()));
+	  this._router.navigate([decodeURI(this._location.path())]);
+	});
+  }
 }
--- ShiftWork.Frontend/src/app/service/task-shift.service.ts
+++ ShiftWork.Frontend/src/app/service/task-shift.service.ts
@@ -1,9 +1,79 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment as env } from '../../environments/environment';
+import { TaskShiftModel } from '../Model/TaskShift';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskShiftService {
 
-  constructor() { }
+  static get parameters() {
+    return [[HttpClient]];
+  }
+  constructor(protected http: HttpClient) {
+    console.log('Hello ClienteServiceProvider Provider');
+  }
+
+  public httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+
+  response: any;
+  data: any;
+  endPoint = 'api/TaskShift/';  //No encontre donde Buscar el nombre del endPoint
+
+  GetTaskShift() {
+    var url = env.apiUrl + this.endPoint;
+    return this.http.get(url);
+  }
+
+  GetTaskShiftID(id:string) {
+    var url =  env.apiUrl + this.endPoint + id;
+    return this.http.get(url)
+      .pipe();
+  }
+
+  PostTaskShift(model: TaskShiftModel)
+  {
+    var url = env.apiUrl + this.endPoint;
+    return this.http.post(url, model, this.httpOptions)
+      .pipe(
+        map( (resp: any) => {
+          console.log(resp);
+          model.taskShiftId = resp.taskShiftId;
+          return model;
+        })
+      );
+  }
+
+  PutTaskShift(model: TaskShiftModel)
+  {
+    //let bodyString = JSON.stringify(model);
+    //console.log(bodyString);
+    var url = env.apiUrl + this.endPoint+ model.taskShiftId;
+    return this.http.put(url,model, this.httpOptions)
+    .pipe(
+      map( (resp: any) => {
+        console.log(resp);
+        //model.taskShiftId = resp.taskShiftId;
+        return model;
+      })
+    );
+  }
+
+  DeleteTaskShift(id: any)
+  {
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    var url = env.apiUrl + this.endPoint + id;
+    return this.http.delete(url);
+  }
+
+  findByName(name: any): Observable<TaskShiftModel[]> {
+    return this.http.get<TaskShiftModel[]>(`${env.apiUrl}?name=${name}`);
+  }
 }
