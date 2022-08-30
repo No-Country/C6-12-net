21c4089a8173ce09cb8bdd6f3331ae51ae8da6f4
williamag929@gmail.com <williamag929@gmail.com>
2022-08-30 11:28:59 -0400

    Nav bar

--- ShiftWork.Frontend/src/app/components/dashboard/nav-bar/nav-bar.component.ts
+++ ShiftWork.Frontend/src/app/components/dashboard/nav-bar/nav-bar.component.ts
@@ -21,7 +21,7 @@ export class NavBarComponent implements OnInit {
       var personId = '1';
       peopleService.GetPerson(personId).subscribe(
         (data) => {
-		this.userLog = data.firstName;
+		//this.userLog = data.firstName;
           console.log('person',data);
         }
       )
