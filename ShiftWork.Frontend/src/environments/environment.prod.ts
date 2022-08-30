f3f83280aed68bc923a9f00efb2e7ed1ccfb3fd2
williamag929@gmail.com <williamag929@gmail.com>
2022-08-30 09:44:50 -0400

    backend https

--- ShiftWork.Frontend/src/environments/environment.prod.ts
+++ ShiftWork.Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl : 'http://44.202.165.91:80/',
+  apiUrl : 'https://shiftwork.serpi.com.co/',
   auth:{
     domain: 'dev-ht5zjk2x.us.auth0.com',
     clientId: 'xX6WLqgD6UgYRjZHKtxD5tlkgwVaqzBk',
